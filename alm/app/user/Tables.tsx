7ebdf95fdb9e2dc83e94b13790b09221124e12f4
Meshack <meshack.odunowe@decagon.dev>
2023-10-23 17:29:54 +0100

    updates

--- alm/app/user/Tables.tsx
+++ alm/app/user/Tables.tsx
@@ -8,16 +8,18 @@ interface TableRow {
   phone: string;
   jobPosition: string;
   role: string;
+  timestamp:number
 }
 
 const Tables: React.FC = () => {
-  const tableData: TableRow[] = [
+  const [tableData, setTableData] = useState<TableRow[]>([
     {
       name: "Ella Smith",
       email: "ella@example.com",
       phone: "123-456-7890",
       jobPosition: "Software Developer",
       role: "Manager",
+      timestamp: new Date("2021-10-01").getTime(),
     },
     {
       name: "Liam Johnson",
@@ -25,6 +27,7 @@ const Tables: React.FC = () => {
       phone: "123-456-7890",
       jobPosition: "Software Developer",
       role: "Manager",
+      timestamp: new Date("2022-03-15").getTime(),
     },
     {
       name: "Olivia Brown",
@@ -32,6 +35,7 @@ const Tables: React.FC = () => {
       phone: "123-456-7890",
       jobPosition: "Software Developer",
       role: "Manager",
+      timestamp: new Date("2020-12-05").getTime(),
     },
     {
       name: "Noah Wilson",
@@ -39,6 +43,7 @@ const Tables: React.FC = () => {
       phone: "123-456-7890",
       jobPosition: "Software Developer",
       role: "Manager",
+      timestamp: new Date("2023-01-20").getTime(),
     },
     {
       name: "Ava Johnson",
@@ -46,6 +51,7 @@ const Tables: React.FC = () => {
       phone: "123-456-7890",
       jobPosition: "Software Developer",
       role: "Manager",
+      timestamp: new Date("2022-09-10").getTime(),
     },
     {
       name: "Mia Davis",
@@ -53,6 +59,7 @@ const Tables: React.FC = () => {
       phone: "987-654-3210",
       jobPosition: "UI Designer",
       role: "Member",
+      timestamp: new Date("2021-11-30").getTime(),
     },
     {
       name: "Lucas Anderson",
@@ -60,6 +67,7 @@ const Tables: React.FC = () => {
       phone: "555-123-4567",
       jobPosition: "Project Manager",
       role: "Manager",
+      timestamp: new Date("2023-02-25").getTime(),
     },
     {
       name: "Sophia Miller",
@@ -67,14 +75,15 @@ const Tables: React.FC = () => {
       phone: "111-222-3333",
       jobPosition: "Software Engineer",
       role: "Member",
+      timestamp: new Date("2020-05-18").getTime(),
     },
-    // Add more entries here
     {
       name: "Emma White",
       email: "emma@example.com",
       phone: "555-555-5555",
       jobPosition: "Product Manager",
       role: "Manager",
+      timestamp: new Date("2022-11-10").getTime(),
     },
     {
       name: "William Davis",
@@ -82,14 +91,15 @@ const Tables: React.FC = () => {
       phone: "111-111-1111",
       jobPosition: "Web Developer",
       role: "Member",
+      timestamp: new Date("2021-12-30").getTime(),
     },
-    // Add more entries as needed
     {
       name: "Aiden Martin",
       email: "aiden@example.com",
       phone: "123-456-7890",
       jobPosition: "Front-end Developer",
       role: "Member",
+      timestamp: new Date("2022-08-10").getTime(),
     },
     {
       name: "Ella Harris",
@@ -97,6 +107,7 @@ const Tables: React.FC = () => {
       phone: "123-456-7890",
       jobPosition: "UI/UX Designer",
       role: "Member",
+      timestamp: new Date("2023-10-05").getTime(),
     },
     {
       name: "Jackson Hall",
@@ -104,6 +115,7 @@ const Tables: React.FC = () => {
       phone: "123-456-7890",
       jobPosition: "Software Engineer",
       role: "Manager",
+      timestamp: new Date("2023-04-15").getTime(),
     },
     {
       name: "Madison King",
@@ -111,6 +123,7 @@ const Tables: React.FC = () => {
       phone: "123-456-7890",
       jobPosition: "Product Manager",
       role: "Manager",
+      timestamp: new Date("2023-10-20").getTime(),
     },
     {
       name: "Liam Turner",
@@ -118,6 +131,7 @@ const Tables: React.FC = () => {
       phone: "123-456-7890",
       jobPosition: "Full-stack Developer",
       role: "Member",
+      timestamp: new Date("2022-07-12").getTime(),
     },
     {
       name: "Olivia Parker",
@@ -125,6 +139,7 @@ const Tables: React.FC = () => {
       phone: "123-456-7890",
       jobPosition: "Graphic Designer",
       role: "Member",
+      timestamp:  new Date("2023-10-19").getTime(),
     },
     {
       name: "Noah Scott",
@@ -132,6 +147,7 @@ const Tables: React.FC = () => {
       phone: "123-456-7890",
       jobPosition: "Data Analyst",
       role: "Manager",
+      timestamp: new Date("2023-03-01").getTime(),
     },
     {
       name: "Isabella Lee",
@@ -139,6 +155,7 @@ const Tables: React.FC = () => {
       phone: "123-456-7890",
       jobPosition: "Project Coordinator",
       role: "Manager",
+      timestamp: new Date("2022-04-22").getTime(),
     },
     {
       name: "James Baker",
@@ -146,6 +163,7 @@ const Tables: React.FC = () => {
       phone: "123-456-7890",
       jobPosition: "Front-end Developer",
       role: "Member",
+      timestamp: new Date("2022-12-14").getTime(),
     },
     {
       name: "Charlotte Phillips",
@@ -153,20 +171,47 @@ const Tables: React.FC = () => {
       phone: "123-456-7890",
       jobPosition: "Database Administrator",
       role: "Manager",
+      timestamp: new Date("2021-03-09").getTime(),
     },
-  ];
+    {
+      name: "Zharlotte Zhillips",
+      email: "charlotte@example.com",
+      phone: "123-456-7890",
+      jobPosition: "Database Administrator",
+      role: "Manager",
+      timestamp: new Date("2023-05-20").getTime(),
+    },
+  ]);
+  
 
   const itemsPerPage = 5;
   const [currentPage, setCurrentPage] = useState(1);
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedRows, setSelectedRows] = useState<Record<string, boolean>>({});
-
+  const [sortType, setSortType] = useState<string | null>(null);
+  
   const toggleRowSelection = (name: string) => {
     setSelectedRows({
       ...selectedRows,
       [name]: !selectedRows[name],
     });
   };
+
+  const sortTable = () => {
+    const sortedData = [...tableData];
+    if (sortType === "New-Old") {
+      sortedData.sort((a, b) => a.timestamp - b.timestamp);
+    } else if (sortType === "Old-New") {
+      sortedData.sort((a, b) => b.timestamp - a.timestamp);
+    } else if (sortType === "A-Z") {
+      sortedData.sort((a, b) => b.name.localeCompare(a.name));
+    } else if (sortType === "Z-A") {
+      sortedData.sort((a, b) => a.name.localeCompare(b.name));
+    }
+    setTableData(sortedData);
+  };
+  
+  
 
   const nextPage = () => {
     setCurrentPage((prevPage) => prevPage + 1);
@@ -187,9 +232,7 @@ const Tables: React.FC = () => {
   return (
     <div className="w-[1094px] mx-auto mt-[40px]">
       <div className="flex gap-[18px] mb-[16px]">
-        <div
-          style={{ position: "relative", width: "686px" }}
-          className="border">
+        <div style={{ position: "relative", width: "686px" }} className="border">
           <input
             type="search"
             name="search"
@@ -228,12 +271,18 @@ const Tables: React.FC = () => {
           <label htmlFor="sort-select">
             <BiSort />
           </label>
-          <select id="sort-select" className="max-w-[186px] px-[16px]">
+          <select
+            id="sort-select"
+            className="max-w-[186px] px-[16px]"
+            onChange={(e) => {
+              setSortType(e.target.value);
+              sortTable();
+            }}
+          >
             <option value="">New - Old</option>
-            <option value="Manager">New-Old</option>
-            <option value="Member">Old - New</option>
-            <option value="Member">A- Z</option>
-            <option value="Member">Z - A</option>
+            <option value="Old-New">Old - New</option>
+            <option value="A-Z">A - Z</option>
+            <option value="Z-A">Z - A</option>
           </select>
         </div>
       </div>
@@ -287,21 +336,22 @@ const Tables: React.FC = () => {
       <div className="w-full flex justify-between mt-[14px]">
         <div>
           <p className="text-[14px] font-[500]">
-            Page {currentPage} of{" "}
-            {Math.ceil(filteredData.length / itemsPerPage)}
+            Page {currentPage} of {Math.ceil(filteredData.length / itemsPerPage)}
           </p>
         </div>
         <div>
           <button
             onClick={prevPage}
             className="rounded-sm py-[8px] px-[14px] border text-[14px] mr-[12px]"
-            disabled={currentPage === 1}>
+            disabled={currentPage === 1}
+          >
             Previous
           </button>
           <button
             onClick={nextPage}
             className="rounded-sm py-[8px] px-[14px] border text-[14px]"
-            disabled={indexOfLastItem >= filteredData.length}>
+            disabled={indexOfLastItem >= filteredData.length}
+          >
             Next
           </button>
         </div>
